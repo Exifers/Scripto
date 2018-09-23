97885a6171513f845c3d09909dfcdb30ded71fbb
Emmanuel Meric de Bellefon <meric.emmanuel@gmail.com>
2018-09-23 14:47:16 +0200

    Added sheebang support

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = g++
-CXXFLAGS = -Wall -Wextra -Werror -pedantic -std=c++17 -I .
+CXXFLAGS = -Wall -Wextra -pedantic -std=c++17 -I .
 
 bin = main
 fls = $(shell find src/ -type f -name "*.cc")
--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,7 @@
 #include <vector>
 #include <exception>
 #include <memory>
+#include <fstream>
 
 #include "lexer.hh"
 #include "parser.hh"
@@ -12,18 +13,34 @@
 
 class Main {
   public:
-    static void run();
+    static void run(std::string source);
+    static std::string read_file(std::string filename);
+    static std::string read_cin();
 };
 
 void
-Main::run() {
+Main::run(std::string source) {
   Lexer lexer = Lexer();
   Printer printer = Printer();
   (void) printer;
   Executer executer = Executer();
-  std::string str;
+  try {
+    auto tokens = lexer.scan(source);
+    Parser parser = Parser(tokens);
+    std::shared_ptr<Node> ast = parser.parse();
+    //ast->accept(printer);
+    //std::cout << std::endl;
+    ast->accept(executer);
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
+std::string Main::read_cin() {
+  std::string source;
   while (true) {
-    str = std::string();
+    std::string str = std::string();
     if (std::cin.peek() == '\n') {
       std::cin.get();
       continue;
@@ -31,24 +48,36 @@ Main::run() {
     else if (std::cin.peek() == EOF) { break; }
 
     std::getline(std::cin, str);
-
-    try {
-      auto tokens = lexer.scan(str); 
-      Parser parser = Parser(tokens);
-      std::shared_ptr<Node> ast = parser.parse();
-      //ast->accept(printer);
-      //std::cout << std::endl;
-      ast->accept(executer);
-    }
-    catch(const std::exception& e) {
-      std::cerr << e.what() << std::endl;
+    if (str[0] != '#') {
+      source += str + "\n";
     }
   }
+  return source;
+}
+
+std::string Main::read_file(std::string filename) {
+  std::ifstream ifs = std::ifstream(filename);
+  std::string source;
+  std::string str;
+  while (ifs.good()) {
+    std::getline(ifs, str);
+    if (str[0] != '#') {
+      source += str + "\n";
+    }
+  }
+  return source;
 }
 
 int main(int argc, char **argv) {
   (void) argc;
   (void) argv;
-  std::cout << "Scripto V0.2" << std::endl;
-  Main::run();
+  std::string source;
+  if (argc >= 2) {
+    source = Main::read_file(argv[1]);
+  }
+  else {
+    std::cout << "Scripto V0.2" << std::endl;
+    source = Main::read_cin();
+  }
+  Main::run(source);
 }
